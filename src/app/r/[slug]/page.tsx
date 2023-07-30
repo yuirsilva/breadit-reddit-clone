0daf952765c59a70d14c6930fe1c6d5b1cc1bbe5
Yuri Silva <yrgamerbr@gmail.com>
2023-07-30 13:05:58 -0300

    create subbreadits

---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import MiniCreatePost from "@/components/MiniCreatePost";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
+import { FC } from "react";
+
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const page: FC<PageProps> = async ({ params }) => {
+  const { slug } = params;
+
+  const session = await getAuthSession();
+
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+
+        take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+      },
+    },
+  });
+
+  if (!subreddit) return notFound();
+
+  return (
+    <>
+      <h1 className="h-14 text-3xl font-bold md:text-4xl">
+        r/{subreddit.name}
+      </h1>
+      <MiniCreatePost session={session} />
+    </>
+  );
+};
+
+export default page;
