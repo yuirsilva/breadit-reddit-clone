f3aa302901556458cc85893010a0b9d48415dc96
Yuri Silva <yrgamerbr@gmail.com>
2023-08-06 20:50:43 -0300

    lmao stop yelling at me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ const Page = async ({}) => {
     <>
       <h1 className="text-3xl font-bold md:text-4xl">Your feed</h1>
       <div className="grid grid-cols-1 gap-y-4 py-6 md:grid-cols-3 md:gap-x-4">
+        {/* @ts-expect-error server component */}
         {session ? <CustomFeed /> : <GeneralFeed />}
 
         {/* subreddit info */}
