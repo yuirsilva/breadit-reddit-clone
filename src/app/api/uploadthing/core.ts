b06a3a7bcba6d77f88c979da69fa60ab96f09acd
Yuri Silva <yrgamerbr@gmail.com>
2023-08-06 20:34:37 -0300

    small fixes

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,18 +1,18 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { getToken } from "next-auth/jwt";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    .middleware(async ({ req }) => {
+    .middleware(async (req) => {
       const user = await getToken({ req });
 
       if (!user) throw new Error("Unauthorized");
 
       return { userId: user.id };
     })
-    .onUploadComplete(async ({}) => {}),
+    .onUploadComplete(async ({ metadata, file }) => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
